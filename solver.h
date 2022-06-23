215464a3a9c45120816f1b13490d0e01e5adaf41
Nathan Nguyendinh <30915871+the6thstar@users.noreply.github.com>
2022-06-23 14:12:52 -0700

    A little cleanup

--- solver.h
+++ solver.h
@@ -1,8 +1,8 @@
 #ifndef SOLVER_H
 #define SOLVER_H
 
-typedef enum Heading {NORTH, EAST, SOUTH, WEST} Heading;
-typedef enum Action {LEFT, FORWARD, RIGHT, IDLE} Action;
+typedef enum Heading { NORTH, EAST, SOUTH, WEST } Heading;
+typedef enum Action { LEFT, FORWARD, RIGHT, IDLE } Action;
 
 struct Cell
 {
@@ -11,21 +11,9 @@ struct Cell
 };
 
 struct Cell* newCell(int x_, int y_);  // Initialize an existing cell with values x and y
-void insertQueue(struct Cell* input);    
+void insertQueue(struct Cell* input);
 void popQueueFront();
 struct Cell* queueFront();
-
-/*
-Rn its capable of initializing the default Manhattan distances and moving in the direction
-of decreasing distance until it can't anymore
-
-Queue is implement (I hope its correct), and I finished the ability to detect and remember walls
-
-Now we just need to implement the pseudocode to be able to 
-recalculate manhattan distances when it gets stuck
-*/
-
-
 
 Action solver();
 Action leftWallFollower();
@@ -39,6 +27,6 @@ int queueStart; //assuming circular queue, this helps us keep track of what posi
 int queueEnd; //keep track of end of queue, where to add next
 
 int horzWall[17][16]; // got rid of the extra in the array to have hopefully less confusion
-int vertWall[16][17]; 
+int vertWall[16][17];
 
 #endif
